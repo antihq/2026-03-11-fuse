24923dbabc2fadd5c5ab94fdaa11e16e4328aaea
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-03-13 06:59:14 -0600

    Update branding with custom logo and favicon assets

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -8,9 +8,9 @@
             <flux:sidebar.header>
                 <flux:sidebar.brand
                     href="#"
-                    logo="https://fluxui.dev/img/demo/logo.png"
-                    logo:dark="https://fluxui.dev/img/demo/dark-mode-logo.png"
-                    name="Acme Inc."
+                    logo="/logo.svg"
+                    logo:dark="/dark-mode-logo.svg"
+                    :name="config('app.name')"
                 />
                 <flux:sidebar.collapse class="in-data-flux-sidebar-on-desktop:not-in-data-flux-sidebar-collapsed-desktop:-mr-2" />
             </flux:sidebar.header>
